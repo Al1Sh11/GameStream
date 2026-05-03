a61098b22fb4ce78822a20695d3bc43ddd83ddc5
Ali Rida El Chaer <42430059@students.liu.edu.lb>
2026-05-03 14:22:00 +0300

    Add base app structure and global styles

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Home        from "./pages/Home";
+import Steam       from "./pages/Steam";
+import Xbox        from "./pages/Xbox";
+import PlayStation from "./pages/PlayStation";
+import Nintendo    from "./pages/Nintendo";
+import Equipment   from "./pages/Equipment";
+import Login       from "./pages/Login";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"            element={<Home />} />
+        <Route path="/steam"       element={<Steam />} />
+        <Route path="/xbox"        element={<Xbox />} />
+        <Route path="/playstation" element={<PlayStation />} />
+        <Route path="/nintendo"    element={<Nintendo />} />
+        <Route path="/equipment"   element={<Equipment />} />
+        <Route path="/login"       element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
